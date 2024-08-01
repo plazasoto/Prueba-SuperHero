29b0e6b69d2ef7799bfca2c4656707ffac426f87
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-01 11:45:20 -0400

    Consultar API parte 1/?

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 $(function(){
-    console.log("ᓀ‸ᓂ");
+    //console.log("ᓀ‸ᓂ");
+
+    /* Validación de la consulta del usuario */
+    // Verificar que se ingrese solamente números
+    function validacionInput(){
+
+    };
+
+    /* Obtener información de la API */
+    const ACCESS_TOKEN = "125466709f3d67e3c8fedb2a691cddd7";
+    function consulaAPI(id){
+        $.ajax({
+            url: `https://superheroapi.com/api.php/${ACCESS_TOKEN}/${id}`,//
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        })
+    };
+
+    //consulaAPI("1");
+
 });
