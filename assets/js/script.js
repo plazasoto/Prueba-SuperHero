3561668d1b309e1cdc165cde3913685b965d1195
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-08-08 23:09:35 -0400

    Gráfico de powerstats.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(function(){
     /* Funcion al hacer click en botón */
     $("#botonBuscar").click(function(event){
         event.preventDefault()
-        //console.log("(ᓀ‸ᓂ)Vanitas vanitatum et omnia vanitas");
+                //console.log("(ᓀ‸ᓂ)Vanitas vanitatum et omnia vanitas");
         console.log($("#inputNumero").val());
         validacionInput($("#inputNumero").val());
     });
@@ -28,7 +28,7 @@ $(function(){
 
     /* Plantilla de resultados */
     function textoResultado(image, nombre, conexiones, publicacion, ocupacion, primera, altura, peso, alias){
-        return `<div class="card mb-3 col-6" style="max-width: 540px;">
+        return `<section class="card mb-3 col-6" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="${image}" class="img-fluid rounded-start" alt="...">
@@ -47,10 +47,10 @@ $(function(){
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-6" id="pieChart">
-                    <img src="./assets/img/sh2.jpg" alt="placeholder" style="width: 100%;">
-                </div>`;
+                </section>
+                <section class="col-6" id="pieChart">
+                    <!--<img src="./assets/img/sh2.jpg" alt="placeholder" style="width: 100%;">-->
+                </section>`;
     }
 
     /* Obtener información de la API */
@@ -74,6 +74,9 @@ $(function(){
                     data.appearance.height,
                     data.appearance.weight,
                     data.biography.aliases));
+                
+                /////////////Probando/////////
+                renderizarGrafico(imprimirDatos(data));
             },
             error: function(error){
                 console.log("Error:");
@@ -84,6 +87,62 @@ $(function(){
         })
     };
 
-    //consulaAPI("1");
+    /* Para recorrer objeto */
+    function imprimirDatos(dato, dataPoints1 = []){
+        ////////////
+        //let dataPoints1 = [];
+        ///////////
+        for(let entry of Object.entries(dato)){
+            if(typeof entry[1] === 'object' && !Array.isArray(entry[1]) && entry[1] !== null){
+                //element.append(`<p>${entry[0]}:</p>`);
+                console.log(`${entry[0]}:`);
+                imprimirDatos(entry[1], dataPoints1);
+            }else{
+                //element.append(`<p>${entry[0]}: ${entry[1]}</p>`)
+                console.log(`${entry[0]}: ${entry[1]}`);
+                switch (entry[0]){
+                    case "intelligence":
+                    case "strength":
+                    case "speed":
+                    case "durability":
+                    case "power":
+                    case "combat":
+                        //console.log("En switch "+entry[0]);
+                        dataPoints1.push({y: parseInt(entry[1]), indexLabel: entry[0]});
+                        console.log(dataPoints1);
+                        break;
+                    default:
+                        //console.log("Not power");
+                        break;
+
+                }
+            }
+        }
+        //////////////////////     
+        return dataPoints1;
+        //////////////////////
+    }
+
+    function renderizarGrafico(datos){
+        let chart = new CanvasJS.Chart("pieChart",
+            {
+                title: {
+                    text: "Estadísticas de Poder"
+                },
+                legend: {
+                    maxWidth: 350,
+                    itemWidth: 120
+                },
+                data: [
+                    {
+                        type: "pie",
+			            showInLegend: true,
+			            legendText: "{indexLabel}",
+                        dataPoints: datos
+                    }
+                ]
+            });
+        chart.render();
+    }
 
 });
